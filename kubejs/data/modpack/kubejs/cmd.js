ebbc88bad89fd5ae92453ef5d0a2f3f024a9de2e
bessiq <bessiq@gmail.com>
2020-07-17 17:19:09 -0400

    simplified

--- kubejs/data/modpack/kubejs/cmd.js
+++ kubejs/data/modpack/kubejs/cmd.js
@@ -3,7 +3,7 @@ events.listen('player.logged_in', function (event) {
 
     event.server.schedule(MINUTE * 5, event.server, function (callback) {
         //plz work idk
-        callback.data.runCommand('/summon yeeter:tnt_yeeter ~ ~ ~ {CustomName:"\"Yee The Inevitable\"",ActiveEffects:[{Id:1,Amplifier:0,Duration:999999},{Id:10,Amplifier:0,Duration:999999},{Id:11,Amplifier:0,Duration:999999},{Id:21,Amplifier:0,Duration:999999}]}')
+        callback.data.runCommand('/summon yeeter:tnt_yeeter ~ ~ ~')
         callback.reschedule()
     })
 })
